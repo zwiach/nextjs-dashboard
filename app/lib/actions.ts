dbcb1ec52567b747bb25115de05509c8089c5344
Volodymyr Basok <volodymyrquo@gmail.com>
2025-01-03 11:03:33 +0200

    Add authorization

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 export type State = {
   errors?: {
     customerId?: string[];
@@ -92,5 +95,23 @@ export async function deleteInvoice(id: string) {
       console.error("Database Error: ", error.message);
 
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
